b8649664b7463b9436efc23afef57b0e24a9e6e6
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-11 11:06:23 +0300

    main.js fix

--- main.js
+++ main.js
@@ -1,37 +1,17 @@
-// 1) Создать переменную num со значением 266219 (тип данных число)
-
 const num = 266219;
-/*Вывести в консоль произведение (умножение) цифр этого числа
 
-Например: число 123, при помощи javaScript получить каждую цифру ( 1, 2, 3 ) и перемножить их. Правильно использовать цикл или методы перебора.*/
+const numArray  = num.toString().split('');
 
+let sum = 1;
 
-numToString  = num.toString();
-numArray = numToString.split('');
-let sum = 0;
-
-for (i = 0; i < numArray.length;) {
+for (i = 0; i < numArray.length; i++) {
     sum *= numArray[i];
-
-    //не знаю в чем ошибка 
-
 };
 
-
-
-
-
-
-
-console.log(numToString);
-console.log(numArray);
-console.log(result);
-
-
-//3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+console.log(sum);
 
 const sumdouble = sum **3;
 
-//4)Вывести в консоль первые 2 цифры полученного числа
+console.log(sumdouble)
 
-console.log(sumdouble.toString().slice(0,1))
+console.log(sumdouble.toString().slice(0,2))
