a0d083755fc01fa8ea75267901abf286bef5b79a
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-12 11:24:44 +0300

    main.js fix

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const num = 266219;
 
-const numArray  = num.toString().split('');
+const numSplitToArray  = num.toString().split('');
 
 let sum = 1;
 
-for (i = 0; i < numArray.length; i++) {
-    sum *= numArray[i];
+for (let i = 0; i < numSplitToArray.length; i++) {
+    sum *= numSplitToArray[i];
 };
 
 console.log(sum);
 
-const sumdouble = sum **3;
+const  cubedResult = sum **3;
 
-console.log(sumdouble)
+console.log( cubedResult)
 
-console.log(sumdouble.toString().slice(0,2))
+console.log( cubedResult.toString().slice(0,2))
